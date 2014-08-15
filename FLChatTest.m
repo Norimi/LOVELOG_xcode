a894eac8ff6d41c2729613561d3fe0eb565ae294
noriming <noriming@gmail.com>
2014-08-15 20:46:50 +0900

    テスト環境構築中

---
+++ FLChatTest.m
@@ -0,0 +1,65 @@
+//
+//  FLChatTest.m
+//  LOVE LOG
+//
+//  Created by netNORIMINGCONCEPTION on 2014/08/14.
+//  Copyright (c) 2014年 norimingconception.net. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "FLChatViewController.h"
+#import "FLChatlogViewController.h"
+
+@interface FLChatTest : XCTestCase
+
+@end
+
+@implementation FLChatTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+    //テスト内容：送信して成功するとcontentsarrayの三十個めが消える
+    //送信成功、reloadすると、ひとつずれた30個が取得される
+    //送った側のidが取得されるidと等しい
+    //idが0のときエラーになる
+    //送ったあとのchatidはもとのchatidより大きい
+   
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+-(void)testContentsArray{
+    
+    
+    FLChatViewController * view = [[FLChatViewController alloc]init];
+    NSMutableArray * contentsArray = [[NSMutableArray alloc]init];
+    contentsArray = [view getContentsArrayFromUserDefaults];
+    [view postandGet];
+    XCTAssertEqual(contentsArray.count,30);
+//    
+//    FLChatlogViewController * send = [[FLChatlogViewController alloc]init];
+//    send.chatField.text = @"test";
+//    [send sendChat];
+//    
+//    [view postandGet];
+//    
+    
+    
+    
+    
+    
+    
+}
+
+@end
---
+++ LOVE LOG Tests/LOVE_LOG_Tests.m
@@ -0,0 +1,34 @@
+//
+//  LOVE_LOG_Tests.m
+//  LOVE LOG Tests
+//
+//  Created by netNORIMINGCONCEPTION on 2014/08/15.
+//  Copyright (c) 2014年 norimingconception.net. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LOVE_LOG_Tests : XCTestCase
+
+@end
+
+@implementation LOVE_LOG_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
